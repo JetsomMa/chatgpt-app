754c48b2123b4ee194e30fe411678a54ed6cc6bf
mashaojie <mashaojie@pinefield-inc.com>
2023-06-28 15:48:18 +0800

    perf: 修复应用刷新报错的bug

--- main.js
+++ main.js
@@ -1,32 +1,10 @@
-// const { app, BrowserWindow } = require('electron')
-
-// function createWindow () {
-//   // 创建浏览器窗口
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     webPreferences: {
-//       nodeIntegration: true
-//     }
-//   })
-
-//   // 加载网站
-//   win.loadURL('https://chat.mashaojie.cn')
-// }
-
-// // 当Electron完成初始化并准备好创建浏览器窗口时调用此方法。
-// app.whenReady().then(() => {
-//   createWindow()
-// })
-
-
 const { app, BrowserWindow } = require('electron')
 
 let mainWindow
 let splashWindow
+let splashWindowClosed = false
 
 function createWindow () {
-  // 创建启动页窗口
   splashWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -36,19 +14,20 @@ function createWindow () {
   })
   splashWindow.loadFile('splash.html')
 
-  // 创建主窗口
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    show: false // 首先隐藏主窗口
+    show: false
   })
 
   mainWindow.loadURL('https://chat.mashaojie.cn')
 
-  // 当主窗口加载完成后，关闭启动页窗口并显示主窗口
   mainWindow.webContents.on('did-finish-load', () => {
-    splashWindow.close()
-    mainWindow.show()
+    if (!splashWindowClosed) {
+      splashWindow.close()
+      splashWindowClosed = true
+      mainWindow.show()
+    }
   })
 }
 
